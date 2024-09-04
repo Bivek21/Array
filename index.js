519b542af7761b11090c2d2574d91c8583a0c913
Bivek Baral <baralbibek1234@gmail.com>
2024-09-04 17:59:42 +0930

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// A variable like structure that can hold more than one value
+
+let fruits = ["apple", "orange", "banana", "coconut"];
+fruits.push("coconut");
+fruits.unshift("mango");
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+//spread operator
+
+// let numbers = [1, 2, 3, 4, 5];
+// let maximum = Math.max(...numbers);
+// let minimum = Math.min(...numbers);
+
+// console.log(maximum);
+// let userName = "Bivek baral";
+// let letters = [...userName].join("-");
+
+// console.log(letters);
